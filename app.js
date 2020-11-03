edaf440c3f06f3d7d0bd2221d5a859f740a623d5
Aytac11 <69673205+Aytac11@users.noreply.github.com>
2020-11-03 12:46:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -30,26 +30,3 @@ submitBtn.addEventListener('click',function(){
 
 })
 
-// function myFunction() {
-//     var x, text;
-  
-//     // Get the value of the input field with id="numb"
-//     x = document.getElementById("numb").value;
-  
-//     // If x is Not a Number or less than one or greater than 10
-//     if (isNaN(x) || x < 1 || x > 10) {
-//       text = "Input not valid";
-//     } else {
-//       text = "Input OK";
-//     }
-//     document.getElementById("demo").innerHTML = text;
-//   }
-
-// validation
-//   function validateForm() {
-//     var x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//       alert("All fields must be filled out");
-//       return false;
-//     }
-//   }
